133919594d5d55e23f510100e7e739ca8095de78
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-01-27 01:49:14 +0530

    add addToHead and Display methods to SinglyLinkedList

---
+++ Data Structures/Implementable Data Structures/Linked List/SinglyLinkedLIst.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
+
+class SinglyLinkedList:
+    head = None
+    tail = None
+
+    def isEmpty(self):
+        if self.head is None :
+            return True
+    
+    def addToHead(self,data):
+        newHead = Node(data) 
+        if self.isEmpty():
+            self.head = newHead
+            self.tail = newHead
+        else:
+            newHead.next = self.head
+            self.head = newHead 
+
+    def Display (self):
+        if self.isEmpty():
+            print("This LinkedList was Empty")
+        else:
+            temp = self.head
+            while temp is not None :
+                print (temp.data , end=" ")
+                temp=temp.next
+        print()
+
+if __name__ =="__main__" :
+
+    linkedList = SinglyLinkedList()
+    linkedList.addToHead(10)
+    linkedList.addToHead(20)
+    linkedList.Display()
+
+
