8c3117f5729c0d3e4e68a7cac534b6c76a66e426
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-01-29 23:09:02 +0530

    added 3 functions in java

--- Data Structures/Implementable Data Structures/Linked List/DoublyLinkedList.java
+++ Data Structures/Implementable Data Structures/Linked List/DoublyLinkedList.java
@@ -1,5 +1,3 @@
-package Implementable Data Structures.Linked List;
-
 public class DoublyLinkedList {
     
 }
---
+++ Data Structures/Implementable Data Structures/Linked List/SinglyLinkedList.java
@@ -0,0 +1,56 @@
+class Node {
+    Object data;
+    Node next;
+
+    public Node (Object data){
+        this.data = data ;
+        this.next = null;
+    }
+
+    public Node (Object data, Node next){
+        this.data = data;
+        this.next = next;
+
+    }
+}
+
+class SinglyLinkedList {
+    Node head ;
+    Node tail ;
+
+    public boolean isEmpty (){
+        return head == null ; 
+    }
+
+    public void addToHead (Object elements){
+        Node newHead = new Node(elements);
+        if (isEmpty()){
+            head = newHead;
+            tail = newHead; 
+        }else {
+            newHead.next = head;
+            head = newHead;
+        }  
+    }
+
+    public void printElement (){
+        if (isEmpty()){
+            System.out.println("Undeflow");
+        }else {
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data + "  ");
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        SinglyLinkedList LinkedList = new SinglyLinkedList();
+        LinkedList.addToHead(10);
+        LinkedList.addToHead("abc");
+        LinkedList.addToHead(100);
+        LinkedList.printElement();
+    }
+}
