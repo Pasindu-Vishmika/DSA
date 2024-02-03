e6477e24b6d3f89bbfd905966df405afb86ce0f5
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-02-04 02:21:25 +0530

    bug fixed

--- Algorithms/Searching Algorithms/Linear Search/LinearSearch.java
+++ Algorithms/Searching Algorithms/Linear Search/LinearSearch.java
@@ -4,11 +4,11 @@ public class LinearSearch {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
 
-            System.out.print("Enter a value =>  ");
+            System.out.print("Enter a Key to find  =>  ");
             int key = sc.nextInt();
 
             int array [] = {1,2,3,4,5,6,7,8,9,10};
-            
+
             if (search(key,array) == -1){
                 System.out.println(key + " is not found in this array");
             }else{
