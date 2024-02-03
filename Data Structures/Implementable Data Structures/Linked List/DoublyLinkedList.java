3bcde3d6455406981f77fc97530bedf57a16a93f
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-02-04 01:29:50 +0530

    added addtoHead and diplay methods

--- Data Structures/Implementable Data Structures/Linked List/DoublyLinkedList.java
+++ Data Structures/Implementable Data Structures/Linked List/DoublyLinkedList.java
@@ -1,3 +1,57 @@
-public class DoublyLinkedList {
+class Node {
     
+    Object data ;
+    Node next;
+    Node prev;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    public static void main(String[] args) {
+        
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        linkedList.addTohead(30);
+        linkedList.addTohead(10);
+        linkedList.addTohead(20);
+        linkedList.addTohead(30);
+        linkedList.DisplayElements();
+        linkedList.addToTail(100);
+        linkedList.addToTail(200);
+        linkedList.DisplayElements();
+    }
+
+    Node head ;
+    Node tail ;
+
+    public boolean isEmpty(){
+        return head==null;
+    }
+    public void addTohead(Object ele){
+        Node newHead = new Node(ele);
+        if (isEmpty()){
+            head = newHead;
+            tail = newHead;
+        }else{
+            newHead.next = head;
+            newHead.prev = null;
+            head.prev = newHead;
+            head = newHead;
+        }
+    }
+
+    public void DisplayElements (){
+        Node temp = head ;
+        while (temp !=null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
 }
