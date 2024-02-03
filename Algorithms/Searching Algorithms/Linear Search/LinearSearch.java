bd8b80df7d28cb6d9f3e2effd9d37e0484e161f6
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-02-04 02:19:45 +0530

    added linearSearch.java

--- Algorithms/Searching Algorithms/Linear Search/LinearSearch.java
+++ Algorithms/Searching Algorithms/Linear Search/LinearSearch.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class LinearSearch {
-    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Enter a value =>  ");
+            int key = sc.nextInt();
+
+            int array [] = {1,2,3,4,5,6,7,8,9,10};
+            
+            if (search(key,array) == -1){
+                System.out.println(key + " is not found in this array");
+            }else{
+                System.out.println(key + " found in index number "+ search(key,array));
+            }
+        }
+    }
+
+    public static int search(int key,int arr []){
+        for (int i = 0 ; i <arr.length; i++ ){
+            if (key == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
